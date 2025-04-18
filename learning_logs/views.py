09ece0cf14e6584d335040af286b7e9bf4253df6
melomatheuss <matheus.syn.eco@gmail.com>
2025-04-18 17:13:50 -0400

    log: init-Topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -12,3 +12,13 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    '''MOSTRA UM UNICO ASSUNTO E TODAS AS SUAS ENTRADAS'''
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {
+        'topic': topic,
+        'entries': entries
+        }
+    return render(request, 'learning_logs/topic.html', context)
